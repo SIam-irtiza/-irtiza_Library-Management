e01cc85edbd65a4ca671b581e3e461ea21f37e27
SIam-irtiza <johayerirtiza@gmail.com>
2026-05-19 00:32:45 +0600

    Created  backend modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
+import { BorrowsModule } from './borrows/borrows.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, BooksModule, BorrowsModule],
   controllers: [AppController],
   providers: [AppService],
 })
